5260561434a5ab310aa74a06ace050b6a3497da9
AhmadAZ2021 <94563696+AhmadAZ2021@users.noreply.github.com>
2025-05-25 21:09:55 +0300

    Update test.py

--- ML_Project/test.py
+++ ML_Project/test.py
@@ -3,11 +3,11 @@ from ultralytics import YOLO
 
 model = YOLO(r"<Model Path>") 
 
-# 🔍 Inference on an image
+# Inference on an image
 img_path = r"<Image Path>"  # replace with your image path
 results = model(img_path, conf=0.1, show=True, device='cpu') 
 results[0].save(filename='predicted_image.jpg')  
 
-# # 🎥 Inference on a video
+# Inference on a video
 # video_path = '<Video Path>'  
 # results = model(video_path, save=True, show=True, device='cpu')  
